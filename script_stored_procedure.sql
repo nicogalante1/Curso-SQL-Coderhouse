1f06a92bc5a058c9f08a4ca383f13c7581a5e82b
Nicola Galante <92265752+nicogalante1@users.noreply.github.com>
2022-09-27 22:26:53 -0300

    Update script_stored_procedure.sql

--- script_stored_procedure.sql
+++ script_stored_procedure.sql
@@ -9,3 +9,15 @@ SET email = CONCAT(operator_name, surname, '@prestapp.com');   -- El mail será 
 SET operator_password =  MD5(CONCAT('prestapp', surname));     -- está función la encontré en internet, "hashea" un texto creando una contraseña única, irrepetible a partir de la palabra "prestapp" y el apellido del operador. No se puede regresar el hash así que no se pueden adivinar las contraseñas al menos que un agente externo conozca los parámetros
 INSERT INTO operator (`email`,`operator_name`,`surname`,`password`) VALUES (email, operator_name,surname,operator_password);   -- Lo inserta en la tabla
 END
+
+--Segundo Script
+--Cambia a estado cerrado a los clientes que en el proceso hayan hecho una denuncia
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `pasar_a_judicial`(in client_id int, in due_amount float)
+BEGIN
+IF due_amount < 500 then
+UPDATE prestapp.loan SET loan_status = '12' WHERE (loan.client_id = client_id);
+else 
+select 'No se pueden cerrar casos que deben 500 o más. Resolver con abogado';
+end IF; 
+END

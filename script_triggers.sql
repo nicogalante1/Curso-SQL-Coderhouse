f2f2404fb9b5b75c76ba791b74e2774511ea5b30
Nicola Galante <92265752+nicogalante1@users.noreply.github.com>
2022-09-29 14:39:15 -0300

    Update script_triggers.sql

--- script_triggers.sql
+++ script_triggers.sql
@@ -11,13 +11,15 @@ CHANGE COLUMN `operator` `operator` VARCHAR(50) NULL ,
 DROP INDEX `operator` ;
 ;
 
---Ahora sí, el trigger se vería así:
+-- Le agregamos una columna que registre la hora, así satisface el requerimiento de la entreg
+ALTER TABLE `prestapp`.`loan_status_history` 
+ADD COLUMN `history_time` TIME NULL DEFAULT NULL AFTER `history_date`;
 
-DROP TRIGGER IF EXISTS log_cambio_status_loan;
+--Ahora sí, el trigger se vería así:
 CREATE TRIGGER log_cambio_status_loan
 BEFORE UPDATE ON loan
 FOR each row
-INSERT INTO loan_status_history (status_history_id, loan_id, operator, status_id, history_date) VALUES (NULL, old.loan_id, user(), NEW.loan_status, NOW());
+INSERT INTO loan_status_history (status_history_id, loan_id, operator, status_id, history_date, history_time) VALUES (NULL, old.loan_id, user(), NEW.loan_status, CURDATE(), CURTIME());
 
 --Con este script, cambiamos un estado en la tabla loans y podemos ver como el trigger registra en loan_status_history
 
